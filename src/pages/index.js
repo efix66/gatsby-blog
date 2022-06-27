2b7ffda6a92fdc283ae304c0f920099277a8c596
Andries Felix <56032612+efix66@users.noreply.github.com>
2022-06-27 18:54:36 +0300

    Finished Gatsby Tutorial Part 2

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,13 @@
 import * as React from "react";
 
+import Layout from "../layout/Layout";
+
 // markup
 const IndexPage = () => {
 	return (
-		<main>
-			<title>Home Page</title>
-			<h1>Welcome to my Gatsby site!</h1>
+		<Layout pageTitle="Home Page">
 			<p>I'm making this by following the Gatsby Tutorial.</p>
-		</main>
+		</Layout>
 	);
 };
 
