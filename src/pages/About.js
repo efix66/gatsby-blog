6964efc78dd106ef311610f7e2b604b32be779a2
Andries Felix <56032612+efix66@users.noreply.github.com>
2022-06-28 10:56:58 +0300

    Finished Gatsby Tutorial Part 5

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,13 +3,13 @@ import React from "react";
 import Layout from "../layout/Layout";
 
 const About = () => {
-	return (
-		<Layout pageTitle="About Me">
-			<p>
-				Hi there! I'm the proud creator of this site, which I built with Gatsby.
-			</p>
-		</Layout>
-	);
+  return (
+    <Layout pageTitle='About Me'>
+      <p>
+        Hi there! I'm the proud creator of this site, which I built with Gatsby.
+      </p>
+    </Layout>
+  );
 };
 
 export default About;
---
+++ src/pages/{mdx.slug}.js
@@ -0,0 +1,12 @@
+import Layout from "../layout/Layout";
+import React from "react";
+
+const BlogPost = () => {
+  return (
+    <Layout pageTitle='Super Cool Blog Posts'>
+      <p>My blog post contents will go here (eventually).</p>
+    </Layout>
+  );
+};
+
+export default BlogPost;
