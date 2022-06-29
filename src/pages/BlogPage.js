ce315ded189e72464a51f40f59b2e1b1fdd721bc
Andries Felix <56032612+efix66@users.noreply.github.com>
2022-06-29 10:04:17 +0300

    Creating pages automaticaly

--- src/pages/BlogPage.js
+++ src/pages/BlogPage.js
@@ -1,28 +1,21 @@
 import React from "react";
-import { graphql } from "gatsby";
+import { graphql, Link } from "gatsby";
 import { MDXRenderer } from "gatsby-plugin-mdx";
 import Layout from "../layout/Layout";
 
 const BlogPage = ({ data }) => {
-  return (
-    <Layout pageTitle='My Blog Posts'>
-      <p style={{ textAlign: "center" }}>My cool blog posts</p>
-      {/* <ul>
-        {data.allFile.nodes.map((node) => (
-          <li>{node.name}</li>
-        ))}
-      </ul> */}
-      <div style={{ display: "flex", gap: 25 }}>
-        {data.allMdx.nodes.map((node) => (
-          <article key={node.id}>
-            <h2>{node.frontmatter.title}</h2>
-            <p>Posted: {node.frontmatter.date}</p>
-            <MDXRenderer>{node.body}</MDXRenderer>
-          </article>
-        ))}
-      </div>
-    </Layout>
-  );
+	return (
+		<Layout pageTitle="My Blog Posts">
+			{data.allMdx.nodes.map((node) => (
+				<article key={node.id}>
+					<h2>
+						<Link to={`./blog/${node.slug}`}>{node.frontmatter.title}</Link>
+					</h2>
+					<p>Posted: {node.frontmatter.date}</p>
+				</article>
+			))}
+		</Layout>
+	);
 };
 
 // export const query = graphql`
@@ -36,18 +29,18 @@ const BlogPage = ({ data }) => {
 // `;
 
 export const query = graphql`
-  query {
-    allMdx(sort: { fields: frontmatter___date, order: ASC }) {
-      nodes {
-        frontmatter {
-          date(formatString: "MMMM D, YYYY")
-          title
-        }
-        id
-        body
-      }
-    }
-  }
+	query {
+		allMdx(sort: { fields: frontmatter___date, order: ASC }) {
+			nodes {
+				frontmatter {
+					date(formatString: "MMMM D, YYYY")
+					title
+				}
+				id
+				body
+			}
+		}
+	}
 `;
 
 export default BlogPage;
