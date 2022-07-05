168c7c9d439698006efe456b1ee36e49b2012d5f
Andries Felix <56032612+efix66@users.noreply.github.com>
2022-07-05 20:06:26 +0300

    bug fix

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -8,7 +8,6 @@ import {
 	navLinkText,
 	siteTitle,
 } from "../components/layout.module.css";
-import NavBar from "../components/navBar/NavBar";
 
 const Layout = ({ pageTitle, children }) => {
 	const data = useStaticQuery(graphql`
@@ -22,43 +21,40 @@ const Layout = ({ pageTitle, children }) => {
 	`);
 	console.log(data);
 	return (
-		<>
-			<NavBar />
-			<div className={container}>
-				<title>
-					{pageTitle} | {data.site.siteMetadata.title}
-				</title>
-				<header className={siteTitle}>{data.site.siteMetadata.title}</header>
-				<nav>
-					<ul className={navLinks}>
-						<li className={navLinkItem}>
-							<Link to="/" className={navLinkText}>
-								Home
-							</Link>
-						</li>
-						<li className={navLinkItem}>
-							<Link to="/About" className={navLinkText}>
-								About
-							</Link>
-						</li>
-						<li className={navLinkItem}>
-							<Link to="/BlogPage" className={navLinkText}>
-								Blog
-							</Link>
-						</li>
-						<li className={navLinkItem}>
-							<Link to="/TestFetch" className={navLinkText}>
-								TestFetch
-							</Link>
-						</li>
-					</ul>
-				</nav>
-				<main>
-					<h1 className={heading}>{pageTitle}</h1>
-					{children}
-				</main>
-			</div>
-		</>
+		<div className={container}>
+			<title>
+				{pageTitle} | {data.site.siteMetadata.title}
+			</title>
+			<header className={siteTitle}>{data.site.siteMetadata.title}</header>
+			<nav>
+				<ul className={navLinks}>
+					<li className={navLinkItem}>
+						<Link to="/" className={navLinkText}>
+							Home
+						</Link>
+					</li>
+					<li className={navLinkItem}>
+						<Link to="/About" className={navLinkText}>
+							About
+						</Link>
+					</li>
+					<li className={navLinkItem}>
+						<Link to="/BlogPage" className={navLinkText}>
+							Blog
+						</Link>
+					</li>
+					<li className={navLinkItem}>
+						<Link to="/TestFetch" className={navLinkText}>
+							TestFetch
+						</Link>
+					</li>
+				</ul>
+			</nav>
+			<main>
+				<h1 className={heading}>{pageTitle}</h1>
+				{children}
+			</main>
+		</div>
 	);
 };
 
--- src/pages/BlogPage.js
+++ src/pages/BlogPage.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { graphql, Link } from "gatsby";
-import { MDXRenderer } from "gatsby-plugin-mdx";
+// import { MDXRenderer } from "gatsby-plugin-mdx";
 import Layout from "../layout/Layout";
 
 const BlogPage = ({ data }) => {
@@ -14,6 +14,32 @@ const BlogPage = ({ data }) => {
 					<p>Posted: {node.frontmatter.date}</p>
 				</article>
 			))}
+			<div
+				className="col-lg-4 col-md-6 col-sm-12 m-b30 wow fadeInDown"
+				data-wow-duration="2s"
+				data-wow-delay="0.3s">
+				<div className="icon-bx-wraper bx-style-2 m-l40 p-a30 left bg-gray icon-bx-ho">
+					<div className="icon-bx-sm bg-primary m-b20">
+						<Link to="#" className="icon-cell">
+							<i className="flaticon-factory-1"></i>
+						</Link>{" "}
+					</div>
+					<div className="icon-content p-l40">
+						<h5 className="dlab-tilte">Institute of Packaging</h5>
+						<p>
+							Lorem ipsum dolor sit amet, consectetuer adipiscing sed diam nibh
+							euismod..
+						</p>
+					</div>
+				</div>
+				<div className="container">
+					<div className="row">
+						<div className=" col-lg-offset-4 col-lg-8">
+							<h1>aksdjgk</h1>
+						</div>
+					</div>
+				</div>
+			</div>
 		</Layout>
 	);
 };
